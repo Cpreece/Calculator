9716a3410bb54f9cf8746799344f9b2e43267b18
cpree <caldonpreece@gmail.com>
2022-08-31 09:25:53 -0600

    finished building the UI

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('help')
+function add(a, b) {
+  return a + b;
+};
+
+function subtract(a,b) {
+  return a - b;
+};
+
+function divide(a,b) {
+  return a / b;
+};
+
+function multiply(a,b) {
+  return a * b;
+};
+
+function operate(func, a, b) {
+  return func(a,b);
+};
